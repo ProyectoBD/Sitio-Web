b76744deb68e219fc5788c167ecba70dad03654d
Fatima Roque Conejo <fatisa_02@hotmail.com>
2015-04-30 08:02:52 -0500

    Primer comit

---
+++ mostrarModificar.php
@@ -0,0 +1,317 @@
+
+<html>
+<head>
+<script language ="javascript" type="text/javascript">
+function calcularSueldo(form1)
+{
+alert();
+alert(form1.horasTrab.value);
+var  hTra=form1.horasTrab.value;
+var  cuoH=form1.cuotaHra.value;
+form1.sueldo.value= hTra*cuoH;
+return  true;
+}
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+<style type="text/css">
+<!--
+#abel {
+	position:absolute;
+	left:4px;
+	top:7px;
+	width:880px;
+	height:101px;
+	z-index:1;
+	background-color: #CC99CC;
+}
+#dante {
+	position:absolute;
+	left:3px;
+	top:110px;
+	width:123px;
+	height:317px;
+	z-index:2;
+	background-color: #CC99CC;
+}
+#altas {
+	position:absolute;
+	left:10px;
+	top:128px;
+	width:92px;
+	height:37px;
+	z-index:3;
+}
+#consultas {
+	position:absolute;
+	left:10px;
+	top:176px;
+	width:109px;
+	height:35px;
+	z-index:4;
+}
+#modificar {
+	position:absolute;
+	left:9px;
+	top:223px;
+	width:110px;
+	height:35px;
+	z-index:5;
+}
+#bajas {
+	position:absolute;
+	left:8px;
+	top:268px;
+	width:111px;
+	height:37px;
+	z-index:6;
+}
+#pPrincip {
+	position:absolute;
+	left:7px;
+	top:315px;
+	width:111px;
+	height:35px;
+	z-index:7;
+}
+#Layer1 {
+	position:absolute;
+	left:130px;
+	top:113px;
+	width:648px;
+	height:302px;
+	z-index:8;
+}
+.Estilo1 {
+	font-size: 36px;
+	font-family: Geneva, Arial, Helvetica, sans-serif;
+}
+#Layer4 {
+	position:absolute;
+	left:129px;
+	top:118px;
+	width:648px;
+	height:296px;
+	z-index:8;
+}
+#Layer5 {
+	position:absolute;
+	left:129px;
+	top:115px;
+	width:647px;
+	height:312px;
+	z-index:8;
+}
+#Layer2 {
+	position:absolute;
+	left:385px;
+	top:275px;
+	width:644px;
+	height:314px;
+	z-index:8;
+}
+#Layer3 {
+	position:absolute;
+	width:941px;
+	height:115px;
+	z-index:1;
+	left: 323px;
+	top: 278px;
+}
+#Layer6 {
+	position:absolute;
+	width:1322px;
+	height:232px;
+	z-index:2;
+	left: -3px;
+	top: 12px;
+	background-color: #0099FF;
+}
+#Layer7 {
+	position:absolute;
+	width:324px;
+	height:833px;
+	z-index:3;
+	left: -27px;
+	top: 12px;
+	background-color: #0099FF;
+}
+#Layer8 {
+	position:absolute;
+	width:189px;
+	height:115px;
+	z-index:4;
+	left: 49px;
+	top: 50px;
+}
+#Layer9 {
+	position:absolute;
+	width:200px;
+	height:52px;
+	z-index:1;
+	left: 51px;
+	top: 268px;
+}
+#Layer10 {
+	position:absolute;
+	width:200px;
+	height:57px;
+	z-index:2;
+	left: 50px;
+	top: 336px;
+}
+#Layer11 {
+	position:absolute;
+	width:200px;
+	height:59px;
+	z-index:3;
+	left: 49px;
+	top: 412px;
+}
+#Layer12 {
+	position:absolute;
+	width:200px;
+	height:58px;
+	z-index:4;
+	left: 21px;
+	top: 489px;
+}
+-->
+</style>
+</head>
+
+<body>
+<div id="Layer2">
+<p>&nbsp;</p>
+<div id="Layer6">
+  <div align="center">
+    <p class="Estilo1">&nbsp;</p>
+    <p class="Estilo1">Nomina Empleados</p>
+    <p><span class="Estilo1">Proyecto</span></p>
+  </div>
+</div>
+<div id="Layer7">
+  <div id="Layer9">
+    <div align="center">
+      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="156" height="59">
+        <param name="BGCOLOR" value="#0099FF">
+        <param name="movie" value="button6.swf">
+        <param name="quality" value="high">
+        <embed src="button6.swf" width="156" height="59" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" bgcolor="#0099FF" ></embed>
+      </object>
+    </div>
+  </div>
+  <div id="Layer10">
+    <div align="center"></div>
+    <div align="center">
+      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="151" height="44">
+        <param name="movie" value="button7.swf">
+        <param name="quality" value="high">
+        <param name="bgcolor" value="#0099FF">
+        <embed src="button7.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="151" height="44" bgcolor="#0099FF"></embed>
+      </object>
+    </div>
+  </div>
+  <div id="Layer11">
+    <div align="center"></div>
+    <div align="center">
+      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="159" height="40">
+        <param name="movie" value="button8.swf">
+        <param name="quality" value="high">
+        <param name="bgcolor" value="#0099FF">
+        <embed src="button8.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="159" height="40" bgcolor="#0099FF"></embed>
+      </object>
+    </div>
+  </div>
+  <div id="Layer8">
+    <div align="center"><img src="../nominaEmpleados_Fatyma/logocecyteg.jpg" width="118" height="121"></div>
+  </div>
+</div>
+<div id="Layer12">
+  <div align="center"></div>
+  <div align="center">
+    <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="147" height="41">
+      <param name="movie" value="button9.swf">
+      <param name="quality" value="high">
+      <param name="bgcolor" value="#0099FF">
+      <embed src="button9.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="147" height="41" bgcolor="#0099FF"></embed>
+    </object>
+  </div>
+</div>
+<? 
+$idEmpleado=$_REQUEST["idEmpleado"]; 
+$conectado=@mysql_connect("localhost","root","1") or die ("no se logro la conexion"); 
+$db=mysql_select_db("nominaempleados"); 
+$strqry="select * from empleado where idEmpleado='".$idEmpleado."'"; 
+$qry=mysql_query($strqry); 
+$i=mysql_num_rows($qry); 
+
+if ($i==0) { 
+?>
+<script language="JavaScript" type="text/javascript"> 
+ alert("El Empleado no existe"); 
+ location.href="modificar.html"; 
+ </script>
+<? 
+ } 
+ else 
+ {
+$r=0; 
+$row=0; 
+mysql_data_seek($qry,$r); 
+$row=mysql_fetch_object($qry); 
+$idEmpleado=$row->idEmpleado; 
+$nombreEmp=$row->nombreEmp; 
+$horasTrab=$row->horasTrab; 
+$cuotaHra=$row->cuotaHra; 
+$sueldo=$row->sueldo; 
+?>
+<div id="Layer2">
+  <p>&nbsp;</p>
+  <form id="form1" name="form1" method="post" action="modificar.php">
+    <table width="758" border="1">
+      <tr>
+        <td width="110">Id Empleado </td>
+        <td width="110"><div align="center">Nombre</div></td>
+        <td width="157"><div align="center">Horas Traabajadas </div></td>
+        <td width="124"><div align="center">Cuota por Hora </div></td>
+        <td width="136"><div align="center">Sueldo</div></td>
+      </tr>
+      <tr>
+        <td><label>
+          <input name="idEmpleado" type="text" id="idEmpleado" value="<? print $row->idEmpleado;?>" readonly="readonly"/>
+        </label></td>
+        <td><label>
+          <input name="nombreEmp" type="text" id="nombreEmp" value="<? print $row->nombreEmp;?>" />
+        </label></td>
+        <td><label>
+          <input name="horasTrab" type="text" id="horasTrab" value="<? print $row->horasTrab;?>"/>
+        </label></td>
+        <td><label>
+          <input name="cuotaHra" type="text" id="cuotaHra" value="<? print $row->cuotaHra;?>"/>
+        </label></td>
+        <td><label>
+          <input name="sueldo" type="text" id="sueldo" value="<? print $row->sueldo;?>" readonly="readonly"//>
+        </label></td>
+      </tr>
+      <tr>
+        <td colspan="5"><div align="center">
+          <label>
+            <input type="button" name="Submit" value="Calcular" onclick='calcularSueldo(this.form)'/ />
+            </label>
+          <label>
+            <input type="submit" name="Submit2" value="Enviar" />
+            </label>
+        </div></td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+  </form>
+  <? 
+mysql_free_result($qry); 
+mysql_close($conectado);
+}
+?>
+</div>
+</body>
+</html>
